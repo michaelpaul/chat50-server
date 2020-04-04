ef2dc08b34ce01702f9f635ebb9064f2af911cfc
Michael Paul <michael@michaelpaul.com.br>
2020-04-04 14:09:07 +0200

    Get channels

--- chat50/api.py
+++ chat50/api.py
@@ -10,12 +10,26 @@ def handle_auth_error(ex):
     response.status_code = ex.status_code
     return response
 
+@bp.route('/channels', methods=['GET'])
+def get_channels():
+    return jsonify([{'key': str(x), 'name': f'Week {x}'} for x in range(9)])
+
 # public
 @bp.route('/messages', methods=['GET'])
 def get_messages():
     return jsonify([
-        {'body': 'foo'}, 
-        {'body': 'bar'}
+        {
+            'author': "Cloud",
+            'avatar': "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+            'body': 'foo',
+            'datetime': 'yesterday'
+        },
+        {
+            'author': "Squall",
+            'avatar': "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+            'body': 'bar',
+            'datetime': 'today'
+        },
     ])
 
 # protected
